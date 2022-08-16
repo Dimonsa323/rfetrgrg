5600c2f2e9edc8a7999edb2c5366380498b58724
Dima Hubenia <dima96gubenya@gmail.com>
2022-08-16 23:57:39 +0300

    Initial Commit

---
+++ rfetrgrg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rfetrgrg
+//
+//  Created by Дима Губеня on 16.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
